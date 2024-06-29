379697b52ce26937518b746d9835a0e1fd9fcf39
JeyasuryaUR <jeyasurya0206@gmail.com>
2024-06-30 00:35:46 +0530

    Deployment configurations

--- ThoughtCapsule/settings.py
+++ ThoughtCapsule/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j3ossl%tie2o5k9qo1+e_5qu$&imm((_qwk2&#s$8p*4pevw!t'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-j3ossl%tie2o5k9qo1+e_5qu$&imm((_qwk2&#s$8p*4pevw!t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['thoughtcapsule.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 CSRF_TRUSTED_ORIGINS = ['https://thoughtcapsule.onrender.com']
 
--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
-python manage.py makemigrations
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
 python manage.py migrate
